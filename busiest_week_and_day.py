966281a44378d8caa7d91c53eec43a23e467caff
jercoh <jejechaber289@msn.com>
2014-08-14 22:09:19 -0700

    weekday fix

--- busiest_week_and_day.py
+++ busiest_week_and_day.py
@@ -3,7 +3,7 @@ import operator
 from datetime import datetime
 
 weekly_commits = {}
-daily_commits = {}
+weekday_commits = {}
 
 def get_busiest_week(commits):
 	json_commits = json.loads(commits)
@@ -15,24 +15,25 @@ def get_busiest_week(commits):
 		else:
 			weekly_commits[week] += 1
 
-def get_busiest_day(commits):
+def get_busiest_weekday(commits):
 	json_commits = json.loads(commits)
 	for commit in json_commits:
 		date_object = datetime.strptime(commit["commit"]["author"]["date"], '%Y-%m-%dT%H:%M:%SZ')
-		if date_object not in daily_commits.keys():
-			daily_commits[date_object] = 1
+		weekday = date_object.strftime('%A')
+		if weekday not in weekday_commits.keys():
+			weekday_commits[weekday] = 1
 		else:
-			daily_commits[date_object] += 1
+			weekday_commits[weekday] += 1
 
 def main():
     commit_file = open(sys.argv[1])
     for line in commit_file:
     	get_busiest_week(line)
-    	get_busiest_day(line)
+    	get_busiest_weekday(line)
     busiest_week = max(weekly_commits.iteritems(), key=operator.itemgetter(1))[0]
-    busiest_day = max(daily_commits.iteritems(), key=operator.itemgetter(1))[0]
+    busiest_weekday = max(weekday_commits.iteritems(), key=operator.itemgetter(1))[0]
     print "Last year, the week that had the greatest number of commits is week #"+str(busiest_week)+"."
-    print "Last year, the day that had the greatest number of commits is "+str(busiest_day.date())+" in week #"+str(busiest_day.isocalendar()[1])+"."
+    print "Last year, the weekday that had the greatest number of commits is "+str(busiest_weekday)+" with "+str(weekday_commits[busiest_weekday])+" commits."
 
 if __name__ == '__main__':
     main()
--- data_to_tsv.py
+++ data_to_tsv.py
@@ -1,7 +1,8 @@
 import json, sys, csv, operator
 from datetime import datetime
 
-result = {}
+day_hour = {}
+weekday = {'Monday':0, 'Tuesday':0, 'Wednesday':0, 'Thursday':0, 'Friday':0, 'Saturday':0, 'Sunday':0}
 
 def extract_data_to_tsv(commits):
     json_commits = json.loads(commits)
@@ -9,24 +10,39 @@ def extract_data_to_tsv(commits):
         date_object = datetime.strptime(commit["commit"]["author"]["date"], '%Y-%m-%dT%H:%M:%SZ')
         day = date_object.isoweekday()
         hour = date_object.hour
+        str_day = date_object.strftime('%A')
+
         if hour == 0:
             hour = 24
-        result[(day, hour)] += 1
+        day_hour[(day, hour)] += 1
+        weekday[str_day] += 1
 
 def main():
     commit_file = open(sys.argv[1])
+    # Initialize dicts
     for day in range(1,8):
         for hour in range(1,25):
-            result[(day, hour)] = 0
+            day_hour[(day, hour)] = 0
+    # Fill dicts
     for line in commit_file:
     	extract_data_to_tsv(line)
-    # Write CSV file
-    myfile = open('data.tsv', 'wb')
-    wr = csv.writer(myfile, delimiter='\t')
-    wr.writerow(['day', 'hour', 'value'])
-    for time in result.keys():
-        mylist = [time[0], time[1], result[time]]
-        wr.writerow(mylist)
+
+    # Write CSV files
+    day_hour_file = open('commit_heatmap.tsv', 'wb')
+    weekday_file = open('commit_per_weekday.tsv', 'wb')
+
+    wr1 = csv.writer(day_hour_file, delimiter='\t')
+    wr2 = csv.writer(weekday_file, delimiter='\t')
+
+    wr1.writerow(['day', 'hour', 'value'])
+    wr2.writerow(['day', 'value'])
+
+    for time in day_hour.keys():
+        mylist = [time[0], time[1], day_hour[time]]
+        wr1.writerow(mylist)
+    for day in weekday.keys():
+        mylist = [day, weekday[day]]
+        wr2.writerow(mylist)
 
 if __name__ == '__main__':
     main()
